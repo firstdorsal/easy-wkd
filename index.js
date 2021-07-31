cf4032d3fd42c0115241090df4a78687f1e98e70
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@firstdorsal.eu>
2021-07-31 23:47:29 +0200

    advanced

--- index.js
+++ index.js
@@ -22,15 +22,18 @@ keyIds.forEach(async keyId => {
 
     // export the keys in the correct format to the right locations
     execSync(`gpg --no-armor --export ${keyId} > public/.well-known/openpgpkey/hu/${wkdHash[2]}`);
-    await fs.mkdir(`public/.well-known/openpgpkey/${wkdHash[3]}/`).catch(e => console.log(e));
-    await fs.copyFile(`public/.well-known/openpgpkey/hu/${wkdHash[2]}`, `public/.well-known/openpgpkey/${wkdHash[3]}/${wkdHash[2]}`);
+    await fs.mkdir(`public/.well-known/openpgpkey/${wkdHash[3]}/hu/`, { recursive: true }).catch(e => console.log(e));
+    await fs.copyFile(`public/.well-known/openpgpkey/hu/${wkdHash[2]}`, `public/.well-known/openpgpkey/${wkdHash[3]}/hu/${wkdHash[2]}`);
 
     console.log(`imported key for id: ${keyId}`);
+    console.log("");
+
     console.log("trying to get the key via wkd (proxy/dns/routing has to work for this):");
     console.log("also check out https://metacode.biz/openpgp/web-key-directory");
-
+    console.log("");
     const test = execSync(`gpg -v --auto-key-locate clear,wkd,nodefault --locate-key ${keyId}`).toString();
     console.log(test);
+    console.log("");
 });
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
